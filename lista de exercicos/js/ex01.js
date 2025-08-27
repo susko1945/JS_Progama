d59f0a985b3ab7bd423ff87d6bea617f263c5517
Luís Henrique <rafaelsusko3113@gmail.com>
2025-08-27 16:15:42 -0300

    lista de exercicios
    atividade1

---
+++ lista de exercicos/js/ex01.js
@@ -0,0 +1,13 @@
+var numero
+
+numero(prompt("digite um número"))
+
+if(numero<=3)
+{
+    numero=numero*3
+    alert("o triplo do valor é" + numero)
+}
+else 
+{
+    alert(" este numero é inválido , tente outro entre 0 e 3 ")
+}
