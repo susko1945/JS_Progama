ad2ad54d8e6d13cd4aa483df5cf3cb94a745de6b
Luís Henrique <rafaelsusko3113@gmail.com>
2025-08-27 15:05:11 -0300

    primeiro escript
    foi colocado a funçao para tirar CNH

---
+++ estruturas_Condicionais/js/ex00.js
@@ -0,0 +1,13 @@
+var nome, idade
+
+nome=prompt("Digite seu nome:")
+idade=parseInt(prompt("digite sua idade"))
+
+if(idade>=18)
+{
+    alert(nome +"Voce já pode tentar tirar a cnh")
+
+}
+else{
+    alert(nome+ " você ainda nao pode tirar a sua CNH")
+}
