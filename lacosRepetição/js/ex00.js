d87055f58ac4c2f7cff5e69d2c5e3a1d98b30148
Luís Henrique <rafaelsusko3113@gmail.com>
2025-08-27 15:52:54 -0300

    LacodeRepetiçao.js
    foi criado um laço de repetição

---
+++ lacosRepetição/js/ex00.js
@@ -0,0 +1,27 @@
+ var cont
+
+ cont= 0 
+
+ while(cont<3){
+    alert("Aborrecendo o Usuario" + cont+ "x")
+    cont++
+ }
+ for(cont=0;cont<5; cont++)
+ {
+     if(cont==0){
+    alert("Clique uma vez!")
+     }
+    else
+    {
+        alert("Continue Cliando!")
+    }
+
+var resposta 
+do{
+  resposta=confirme("deseja continuar?")
+}while(resposta==true)
+
+
+
+
+}
