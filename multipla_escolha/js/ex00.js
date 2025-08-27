8ecfe4c9a73853bc9c934fdfd10a00a382456345
Luís Henrique <rafaelsusko3113@gmail.com>
2025-08-27 15:17:38 -0300

    dias da semana sorteio
    sorteio  dos dias da semana

---
+++ multipla_escolha/js/ex00.js
@@ -0,0 +1,36 @@
+var dia
+
+dia = parseInt(prompt("Digite o numero do dia correspondente da semana:"))
+
+switch (dia) {
+    case 1:
+        alert("Domingo")
+        break;
+
+    case 2 :
+        alert("Segunda-Freira")
+        break;
+
+    case 3:
+        alert("Terça_Freira")
+        break;
+     case 4:
+        alert("SQuarta-Freira")
+        break;
+     case 5 :
+        alert("Quinta-Freira")
+        break;  
+    case 6 :
+        alert("Sexta - Freira")
+        break;
+    case7 :
+        alert("sabado")
+        break;
+
+
+
+       
+  default:{
+   alert("opção inválida")
+    }
+}
